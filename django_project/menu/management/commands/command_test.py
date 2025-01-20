04e85f5d9f5d27e86e8da521ab84ed87a4de61a7
y-matsuno-u <chyu24003@g.nihon-u.ac.jp>
2025-01-20 13:54:16 +0900

    読み込み機能修正

--- django_project/menu/management/commands/command_test.py
+++ django_project/menu/management/commands/command_test.py
@@ -7,31 +7,21 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         #読み込みたいファイルのあるディレクトリを指定 
-        directory = "media/bible/books.txt"
+        directory = "media/bible/JCO.txt"
 
         try:
             with open(directory, "r", encoding="utf-8") as file:
 
-                c = 0
-        
                 for line in file:
-            
-                    if not ":" in line:
 
+                    #聖句じゃない行は無視する            
+                    if not ":" in line:
                         continue
 
                     else:
-
                         content = line.split(" ")
-                        #print(content)
-                        #print(content[1])
-                        #print(content[1].split(":"))
                         chapter_and_verse = content[1].split(":")
-                        #print(chapter_and_verse)
                         print(f"{content[2]}の{chapter_and_verse[0]}章{chapter_and_verse[1]}節は「{content[-2]}」です。")
 
-                    c += 1
-                    if c == 2000:
-                        break
         except FileNotFoundError:
             print(f"{directory}が見つかりません")
